006229c709457d851c5feca133cb5007194ec0f9
W00SUNGLEE <vhxmfltm135@gmail.com>
2022-08-25 17:24:19 +0900

    39. 3289

---
+++ 3289/3289.py
@@ -0,0 +1,35 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+
+score = list(map(int, sys.stdin.readline().split()))
+
+answer = 99
+check = -1
+
+for i in range(1, 101):
+    check_score = [0 for _ in range(n)]
+    tmp_check = 0
+
+    for j in range(n):
+        if score[j] >= i:
+            check_score[j] = 1
+
+            if j-1 >= 0:
+                check_score[j-1] = 1
+
+            if j+1 < n:
+                check_score[j+1] = 1
+
+    tmp_check = sum(check_score)
+
+    if check < tmp_check <= k:
+        answer = i
+        check = tmp_check
+
+    elif check == tmp_check and tmp_check <= k:
+        if i > answer:
+            answer = i
+        check = tmp_check
+
+print(answer)
