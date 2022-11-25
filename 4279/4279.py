5a2da0eb59338b4c5b19271f289a06fb43569545
W00SUNGLEE <vhxmfltm135@gmail.com>
2022-11-25 15:16:10 +0900

    154. 4279

---
+++ 4279/4279.py
@@ -0,0 +1,18 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+student_list = list(map(int, sys.stdin.readline().split()))
+
+dp = [0 for _ in range(n)]
+dp[0] = 1
+
+for i in range(1, n):
+    dp[i] = 1
+
+    for j in range(i):
+        if student_list[j] < student_list[i] and dp[j] + 1 > dp[i]:
+            dp[i] = dp[j] + 1
+
+print(dp)
+print(n - max(dp))
