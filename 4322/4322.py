0bac594398644eaad0f64e3d2e39d365b72fbb17
W00SUNGLEE <vhxmfltm135@gmail.com>
2023-01-01 21:10:12 +0900

    196. 4322

---
+++ 4322/4322.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+student_list = list()
+
+for i in range(n):
+    name, score = sys.stdin.readline().split()
+
+    student_list.append([int(score), name])
+
+student_list.sort()
+
+for i in range(n):
+    print(student_list[i][1], end=" ")
